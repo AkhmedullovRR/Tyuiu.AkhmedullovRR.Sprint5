8ed1369a80ed52842dd88b5e8621031c31267f1e
Равиль <fullbotrr@mail.ru>
2024-11-27 01:11:32 +0300

    rfef

--- Tyuiu.AkhmedullovRR.Sprint5.Task0.V27.Test/DataServiceTest.cs
+++ Tyuiu.AkhmedullovRR.Sprint5.Task0.V27.Test/DataServiceTest.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.AkhmedullovRR.Sprint5.Task0.V27.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\Äàøà\source\repos\Tyuiu.KulkoDA.Sprint5\Tyuiu.KulkoDA.Sprint5.Task0.V27\bin\Debug\OutPutFileTask0.txt";
+            string path = @"C:\Users\Äàøà\source\repos\Tyuiu.AkhmedullovRR.Sprint5\Tyuiu.AkhmedullovRR.Sprint5.Task0.V27\bin\Debug\OutPutFileTask0.txt";
             FileInfo fileinfo = new FileInfo(path);
             bool fileExist = fileinfo.Exists;
             bool wait = true;
