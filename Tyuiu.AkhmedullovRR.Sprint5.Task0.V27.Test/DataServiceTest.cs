a8ede52133f2bdd01ccd42611107dd99c9b734ba
Равиль <fullbotrr@mail.ru>
2024-11-27 01:04:32 +0300

    Add project files.

---
+++ Tyuiu.AkhmedullovRR.Sprint5.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AkhmedullovRR.Sprint5.Task0.V27.Lib;
+namespace Tyuiu.AkhmedullovRR.Sprint5.Task0.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Äàøà\source\repos\Tyuiu.KulkoDA.Sprint5\Tyuiu.KulkoDA.Sprint5.Task0.V27\bin\Debug\OutPutFileTask0.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExist = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+
+        }
+    }
+}
