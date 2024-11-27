3ea4bed555f74e01a5fd0f208c478a261381dbb0
Равиль <fullbotrr@mail.ru>
2024-11-27 03:22:07 +0300

    Task2

---
+++ Tyuiu.AkhmedullovRR.Sprint5.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.AkhmedullovRR.Sprint5.Task2.V19.Lib;
+
+namespace Tyuiu.AkhmedullovRR.Sprint5.Task2.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\Àëåêñåé Øìåë¸â\source\repos\Tyuiu.AkhmedullovRR.Sprint5\Tyuiu.AkhmedullovRR.Sprint5.Task2.V15\bin\Debug\net8.0\OutPutFileTask2.csv";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
