0fd581297362ff846ffb3e6f200aefdc186f9555
Равиль <fullbotrr@mail.ru>
2024-11-27 03:04:31 +0300

    jnkkln

---
+++ Tyuiu.AkhmedullovRR.Sprint5.Task1.V21/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.AkhmedullovRR.Sprint5.Task1.V21.Lib;
+namespace Tyuiu.AkhmedullovRR.Sprint5.Task1.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Ахмедуллов Р.Р. | ИСТН-б-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема:                                                                   *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Ахмедуллов Р.Р.                    | ИСТН-б-24-1              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
